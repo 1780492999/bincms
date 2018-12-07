e85aab07661505a660ebfa91f19d24d283da5eda
1780492999 <1780492999@qq.com>
2018-12-07 09:38:28 +0800

    thinkphp

---
+++ runtime/temp/e15d337bba237445fa4f8f53909b5ca9.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:63:"D:\Web\bincms\public/../application/index\view\index\index.html";i:1544087487;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
